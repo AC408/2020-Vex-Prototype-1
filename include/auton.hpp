0fdf513c8468c8ef8188e4c807b7378261b38338
Allen Chen <allenchen799@gmail.com>
2020-12-02 22:57:39 -0500

    cleaned up unnecessary autons

--- include/auton.hpp
+++ include/auton.hpp
@@ -1,11 +1,6 @@
 #ifndef AUTON_HPP
 #define AUTON_HPP
 
-void forward_time(double volt, double time);
-void turn_time(double volt, double time);
-void convey_time(double volt, double time);
-void intake_time(double volt, double time);
 void drive_control(void *);
-
 
 #endif
